eeac1700c62009e666e228acc44db8170633044f
Rishab8077 <rishabbothra56@gmail.com>
2021-04-25 02:02:53 +0545

    Adding votes count

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from .models import Question
 # Create your views here.
 def index(request):
@@ -12,7 +13,17 @@ def details(request, question_id):
     return render(request,'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse('This is results of the questions %s'% question_id)
+    question = get_object_or_404(Question,pk = question_id)
+    return render(request, 'polls/results.html',{'question':question})
 
 def vote(request, question_id):
-    return HttpResponse('votes on questions are %s'% question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk= request.POST['choice'])
+    except:
+        return render(request, 'polls/results.html', {'question': question, 'error_message':'please select a choice'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
