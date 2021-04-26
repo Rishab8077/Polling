9643797012c985a90a378080d76014f1e7559383
Rishab8077 <rishabbothra56@gmail.com>
2021-04-26 13:35:34 +0545

    Desiging webpage

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,10 +5,10 @@ app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     #127.0.0.1:8000/polls
-    url(r'^(?P<question_id>[0-9]+)/$',views.details,name='Details'),
+    url(r'^(?P<question_id>[0-9]+)/$',views.details,name='detail'),
     #127.0.0.1:8000/polls/1
     url(r'^(?P<question_id>[0-9]+)/results$',views.results,name='results'),
     #127.0.0.1:8000/polls/1/results
-    url(r'^(?P<question_id>[0-9]+)/vote$',views.vote,name='Votes'),
+    url(r'^(?P<question_id>[0-9]+)/vote$',views.vote,name='votes'),
     #127.0.0.1:8000/polls/1/vote
 ]
